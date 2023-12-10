6887531e4c6dfe0b462d74c9dd93b0f87585abbf
Yulya <Kokutinayulya2001@gmail.com>
2023-12-10 16:46:59 +0200

    Создала массив из строк и инициализировала переменные для подсчета количества строк

--- control/Program.cs
+++ control/Program.cs
@@ -1,27 +1,3 @@
 ﻿string[] array = { "Hello", ").&", "world", "Cg", "Hoi" };
 int count = 0;
 int n = 0;
-
-for (int i = 0; i < array.Length; i++)
-{
-    // string m = a[i];
-    if (array[i].Length <= 3)
-    {
-        count ++; // считаем сколько раз встречается строка нужного размера
-    }
-}
-
-
-string[] new_array = new string[count]; // создаем новый список размер, которого соответствует количеству строк в массиве заданной длины (меньше или равно 3)
-
-for (int i = 0; i < array.Length; i++)
-{
-    // string m = a[i];
-    if (array[i].Length <= 3)
-    {
-        new_array[n] = array[i];             
-        Console.Write(string.Join(" ,", new_array));
-        
-        
-    }
-}
