cdfb1a5d756237c9f63aed21e9b03955cd1acb04
Yulya <Kokutinayulya2001@gmail.com>
2023-12-10 15:09:26 +0200

    Добавила цикл для подсчета количества строк нужного размера

--- control/Program.cs
+++ control/Program.cs
@@ -2,3 +2,11 @@
 int count = 0;
 int n = 0;
 
+for (int i = 0; i < array.Length; i++)
+{
+    // string m = a[i];
+    if (array[i].Length <= 3)
+    {
+        count ++; // считаем сколько раз встречается строка нужного размера
+    }
+}
